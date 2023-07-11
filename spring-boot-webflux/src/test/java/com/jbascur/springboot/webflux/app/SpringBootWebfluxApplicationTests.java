8daa9022b0a59297b2208f4a87f019d5d96cbe8a
NaojiNerette <naojimagot@gmail.com>
2023-07-11 12:50:10 -0400

    proyecto webflux

---
+++ spring-boot-webflux/src/test/java/com/jbascur/springboot/webflux/app/SpringBootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbascur.springboot.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
